5e1c52f1b277d8c12109d119879573846aaa6f2e
Borja Tur <borja.tur@outlook.es>
2016-07-21 14:04:09 +0200

    first commit

---
+++ testPartialView/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testPartialView.Startup))]
+namespace testPartialView
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
